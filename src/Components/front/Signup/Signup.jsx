8d803659683ffc3da9afd92eb2ba5391177137a6
Soe Arkar Maung <148836349+Arkar08@users.noreply.github.com>
2024-02-11 08:52:35 +0630

    sign add

--- src/Components/front/Signup/Signup.jsx
+++ src/Components/front/Signup/Signup.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import './Signup.css';
 
 const Signup = () => {
   return (
-    <div>Signup</div>
+    <div className='sign-container'>
+      <h2 className='shop'>Shopping</h2>
+      <div className='input-tag'>
+        <div className="name">
+          <label htmlFor="name">Name</label>
+          <input type="text" name='name'/>
+        </div>
+        <div className="name">
+            <label htmlFor="email">Email</label>
+            <input type="text" name='email' />
+        </div>
+        <button className='sign'>Sign up</button>
+      </div>
+    </div>
   )
 }
 
-export default Signup
+export default Signup;
